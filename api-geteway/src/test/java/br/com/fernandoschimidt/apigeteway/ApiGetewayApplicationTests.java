677a136091043a80882765a299f8612c5fc2bb68
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2025-01-07 18:10:17 -0300

    api-geteway

---
+++ api-geteway/src/test/java/br/com/fernandoschimidt/apigeteway/ApiGetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernandoschimidt.apigeteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
