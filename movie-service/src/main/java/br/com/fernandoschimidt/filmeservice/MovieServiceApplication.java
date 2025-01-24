acf142947e3c442c98f405c1afdba204b992916e
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2025-01-23 23:13:07 -0300

    movies

--- movie-service/src/main/java/br/com/fernandoschimidt/filmeservice/MovieServiceApplication.java
+++ movie-service/src/main/java/br/com/fernandoschimidt/filmeservice/MovieServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FilmeServiceApplication {
+public class MovieServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FilmeServiceApplication.class, args);
+		SpringApplication.run(MovieServiceApplication.class, args);
 	}
 
 }
